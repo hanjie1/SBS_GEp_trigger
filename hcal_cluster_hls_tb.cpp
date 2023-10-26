626e904731e3b0edc9b5f47936f06e4c53addd87
Hanjie Liu <hanjie@phallahanjie-mac.jlab.org>
2023-10-26 16:39:33 -0400

    add a function to generate one random cluster

--- hcal_cluster_hls_tb.cpp
+++ hcal_cluster_hls_tb.cpp
@@ -2,13 +2,130 @@
 #include <stdlib.h>
 #include "hcal_cluster_hls.h"
 
-fadc_hits_t GenRandomClusters(){
-  // generate a random list seed
-  // generate a random cluster around each seed 
-  // generate a random non cluster hits around some of the seed
-  // record each cluster and the fiber output
 
+cluster_t GenRandomClusters(ap_uint<13> seed_threshold, ap_uint<3> hit_dt, fadc_hits_t &a_cluster_pre, fadc_hits_t &a_cluster_cur, fadc_hits_t &a_cluster_aft){
+
+  cluster_t cc;
+
+  // generate a seed
+  int seed_ch = rand()%288;
+  ap_uint<13> seed_e = rand()%8192 + seed_threshold;
+  ap_uint<3> seed_t = rand()%8;
+
+  if(seed_ch<256){
+     a_cluster_cur.vxs_ch[seed_ch].e = seed_e;
+     a_cluster_cur.vxs_ch[seed_ch].t = seed_t;
+  }
+  else{
+     a_cluster_cur.fiber_ch[seed_ch-256].e = seed_e;
+     a_cluster_cur.fiber_ch[seed_ch-256].t = seed_t;
+  }
+  
+  block_coords seed_pos;
+  seed_pos = block_map[seed_ch];
+
+  cc.x = seed_pos.nx;
+  cc.y = seed_pos.ny;
+  cc.t = seed_t;
+  
+  // generate a random cluster around each seed 
+
+// 3*3 clusters
+/*
+   |---|---|---|
+   | 0 | 1 | 2 |
+   |---|---|---|
+   | 3 | S | 4 |
+   |---|---|---|
+   | 5 | 6 | 7 |
+   |---|---|---|
+*/
+  int nhits = rand()%8;
+  ap_uint<4> tot = 1;
+  ap_uint<13> tot_e = seed_e;
+
+  for(int ii=0; ii<nhits; ii++){
+   int nblock = rand()%8;
+   switch(nblock){
+    case 0:
+       hit_pos_x = seed_pos.nx-1;
+       hit_pos_y = seed_pos.ny-1;   
+    case 1:
+       hit_pos_x = seed_pos.nx;
+       hit_pos_y = seed_pos.ny-1;   
+    case 2:
+       hit_pos_x = seed_pos.nx+1;
+       hit_pos_y = seed_pos.ny-1;   
+    case 3:
+       hit_pos_x = seed_pos.nx-1;
+       hit_pos_y = seed_pos.ny;   
+    case 4:
+       hit_pos_x = seed_pos.nx+1;
+       hit_pos_y = seed_pos.ny;   
+    case 5:
+       hit_pos_x = seed_pos.nx-1;
+       hit_pos_y = seed_pos.ny+1;   
+    case 6:
+       hit_pos_x = seed_pos.nx;
+       hit_pos_y = seed_pos.ny+1;   
+    case 7:
+       hit_pos_x = seed_pos.nx+1;
+       hit_pos_y = seed_pos.ny+1;   
+   }
+   if(hit_pos_x==0 || hit_pos_x>24 || hit_pos_y==0 || hit_pos_y>12)
+     continue;
+   
+   hit_ch = (hit_pos_x-1)*12 + hit_pos_y-1 ;
+
+   ap_uint<13> ee = rand()%seed_threshold;
+   int dt = rand()%(2*hit_dt)-hit_dt;
+
+   if( (seed_t+dt)<0 ){
+     ap_uint<3> tt = seed_t+dt+8;
+     if( hit_ch<256){
+       a_cluster_pre.vxs_ch[hit_ch].e = ee;
+       a_cluster_pre.vxs_ch[hit_ch].t = tt;
+     }
+     else{
+       a_cluster_pre.fiber_ch[hit_ch-256].e = ee;
+       a_cluster_pre.fiber_ch[hit_ch-256].t = tt;
+     }
+   }
+   else if( (seed_t+ dt)>7 ){
+     ap_uint<3> tt = dt + seed_t-8;
+     if( hit_ch<256){
+       a_cluster_aft.vxs_ch[hit_ch].e = ee;
+       a_cluster_aft.vxs_ch[hit_ch].t = tt;
+     }
+     else{
+       a_cluster_aft.fiber_ch[hit_ch-256].e = ee;
+       a_cluster_aft.fiber_ch[hit_ch-256].t = tt;
+     }
+   }
+   else{
+     ap_uint<3> tt = dt + seed_t;
+     if( hit_ch<256){
+       a_cluster_cur.vxs_ch[hit_ch].e = ee;
+       a_cluster_cur.vxs_ch[hit_ch].t = tt;
+     }
+     else{
+       a_cluster_cur.fiber_ch[hit_ch-256].e = ee;
+       a_cluster_cur.fiber_ch[hit_ch-256].t = tt;
+     }
+   }
+
+   tot = tot+1;   
+   tot_e = tot_e + ee;
+  } 
+
+  cc.e = tot_e;
+  cc.nhits = tot;
+
+  return cc;  
 }
+
+// generate a random non cluster hits around some of the seed
+// record each cluster and the fiber output
 
 int main(int argc, char *argv[])
 {
@@ -20,29 +137,49 @@ int main(int argc, char *argv[])
 
   srand(10);
 
-  // generate some random FADC hits
-  fadc_hits_t fadc_hits;
 
-  for(int ch=0; ch<256; ch++){
-      fadc_hits.vxs_ch[ch].e = 0;
-      fadc_hits.vxs_ch[ch].t = 0;
+  fadc_hits_t pre_pre_hits;
+  fadc_hits_t pre_hits;
+  for(int frame=0; frame<6; frame++){
+      fadc_hits_t new_hits;
+      
+      for(int ii=0; ii<256; ii++){
+         new_hits.vxs_ch[ii].e=0
+         new_hits.vxs_ch[ii].t=0
+      }
+
+      for(int ii=0; ii<32; ii++){
+         new_hits.fiber_ch[ii].e=0
+         new_hits.fiber_ch[ii].t=0
+      }
+
+      // generate random hits below seed_threshold
+      int nhits = rand()%10;
+      for(int ii=0; ii<nhits; ii++){
+          int rd_ch = rand()%288;
+          ap_uint<13> ee = rand()%seed_threshold;
+          ap_uint<3> tt = rand()%8;
+
+          if(seed_ch<256){
+             new_hits.vxs_ch[rd_ch].e = ee;
+             new_hits.vxs_ch[rd_ch].t = tt;
+          }
+          else{
+             new_hits.vxs_ch[rd_ch-256].e = ee;
+             new_hits.vxs_ch[rd_ch-256].t = tt;
+          }
+      }
+
+      if(frame>=2){
+         // generate one cluster 
+         cluster_t newcc;
+         newcc=GenRandomClusters(seed_threshold, hit_dt, fadc_hits_t &pre_pre_hits, fadc_hits_t &pre_hits, fadc_hits_t &new_hits);
+
+      }
+
   }
+  
 
-  for(int ch=0; ch<32; ch++){
-      fadc_hits.fiber_ch[ch].e = 0;
-      fadc_hits.fiber_ch[ch].t = 0;
-  }
-
-  fadc_hits.vxs_ch[5].e=3000;
-  fadc_hits.vxs_ch[5].t=2;
-  fadc_hits.vxs_ch[10].e=6600;
-  fadc_hits.vxs_ch[10].t=1;
-  fadc_hits.vxs_ch[11].e=3500;
-  fadc_hits.vxs_ch[11].t=3;
-  fadc_hits.fiber_ch_l[1].e=2400;
-  fadc_hits.fiber_ch_l[1].t=3;
-  fadc_hits.fiber_ch_l[2].e=3500;
-  fadc_hits.fiber_ch_l[2].t=2;
   s_fadc_hits.write(fadc_hits);
 
 /*
