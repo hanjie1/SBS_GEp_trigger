a1f92488c911ef8950ba59077e5e38ecf91518da
hanjie1 <hanjie@jlab.org>
2023-12-01 15:16:45 -0500

    fix some bugs

--- hcal_cluster_hls_tb.cpp
+++ hcal_cluster_hls_tb.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fstream>
+#include <string>
 #include "hcal_cluster_hls.h"
+using namespace std;
 
 int main(int argc, char *argv[])
 {
@@ -17,15 +20,15 @@ int main(int argc, char *argv[])
       fadc_hits_t new_hits;
 
       char filename[100];
-      sprintf(filename, "%s%d.txt","data/frame",ii);
+      snprintf(filename, 100, "%s%d.txt","data/frame",ii);
       ifstream infile(filename);
 
-      string line;
       int nn=0;
-      while( getline( infile, line ) )
+      while( !infile.eof() )
       {
          int ee,tt;
-         sscanf(line, "%d  %d",ee,tt);
+         infile>>ee;
+         infile>>tt;
          if(nn<256){
             new_hits.vxs_ch[nn].e = (ap_uint<14>)ee;
             new_hits.vxs_ch[nn].t = (ap_uint<3>)tt;
@@ -56,12 +59,12 @@ int main(int argc, char *argv[])
   while(!s_cluster_all.empty()){
      cluster_all_t cc = s_cluster_all.read();
      for(int ii=0; ii<288; ii++){
-         if(cc[ii].e>0){
-            tmpe = cc[ii].e.to_uint();
-            tmpt = cc[ii].t.to_uint()*4+nn*8*4;
-            tmpx = cc[ii].x.to_uint();
-            tmpy = cc[ii].y.to_uint();
-            tmpn = cc[ii].nhits.to_uint();
+         if(cc.c[ii].e>0){
+            int tmpe = cc.c[ii].e.to_uint();
+            int tmpt = cc.c[ii].t.to_uint()*4+nn*8*4;
+            int tmpx = cc.c[ii].x.to_uint();
+            int tmpy = cc.c[ii].y.to_uint();
+            int tmpn = cc.c[ii].nhits.to_uint();
 
             printf("Find cluster at frame %d: ch=%d, e=%d, t=%d, x=%d, y=%d, nhits=%d\n",nn,ii,tmpe,tmpt,tmpx,tmpy,tmpn);
          }
@@ -73,8 +76,8 @@ int main(int argc, char *argv[])
   while(!s_fiberout.empty()){
      fiber_bins_t ff = s_fiberout.read();
      for(int ii=0; ii<128; ii++){
-         if(ff[ii].valid==1){
-            tmpt = cc[ii].t.to_uint()*4+nn*8*4;
+         if(ff.bins[ii].valid==1){
+            int tmpt = ff.bins[ii].t.to_uint()*4+nn*8*4;
 
             printf("Fiber out at frame %d: ch=%d, t=%d\n",nn,ii,tmpt);
          }
